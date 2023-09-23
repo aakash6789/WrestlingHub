0d3324d95fdd480d2e55a252f78615b26cbc97b6
Aakash <aakash2506c@gmail.com>
2023-09-23 23:13:45 +0530

    Working on superstars

--- src/components/SuperStars.jsx
+++ src/components/SuperStars.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import superstarbanner from '../assets/UNIVERSAL_CHAMPIONSHIP_BANNER.jpg'
 const SuperStars = () => {
   return (
-    <div>
-      Supertars
+    <div className=' h-[1000px] font-bebasNeue bg-black text-white'>
+      <img src={superstarbanner} className='absoloute z-[-4] opacity-[0.8]'/>
+      <h1 className='text-8xl absolute top-[320px] right-[200px] text-yellow-200'>SUPERSTARS</h1>
+      <div className='ml-[700px] mt-[40px] text-3xl mr-[695px]'>WWE ROSTER</div>
     </div>
   )
 }
