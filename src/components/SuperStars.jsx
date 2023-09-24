6b1063b2f52f04bc750b35ebf8d6ed9530e65e0f
Aakash <aakash2506c@gmail.com>
2023-09-24 18:18:33 +0530

    working on mapping superstars

--- src/components/SuperStars.jsx
+++ src/components/SuperStars.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import superstarbanner from '../assets/UNIVERSAL_CHAMPIONSHIP_BANNER.jpg'
+import superStarTile from '../assets/SuperStar_tile3.png';
 const SuperStars = () => {
   return (
-    <div className=' h-[1000px] font-bebasNeue bg-black text-white'>
+    <div className=' font-bebasNeue bg-black text-white'>
       <img src={superstarbanner} className='absoloute z-[-4] opacity-[0.8]'/>
       <h1 className='text-8xl absolute top-[320px] right-[200px] text-yellow-200'>SUPERSTARS</h1>
       <div className='ml-[700px] mt-[40px] text-3xl mr-[695px]'>WWE ROSTER</div>
+      <div className='p-[10px] mr-[1300px] ml-[30px] bg-white text-black hover:bg-yellow-300 hover:text-black transition duration-0 hover:duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
+        <img src={superStarTile} className=' h-[300px] w-[200px] rounded-[8px] xs:h-[200px] w-[200px]'/>
+       <span className='md:ml-[58px] xs:ml-[35px]'>AJ STYLES</span>
+       </div>
+      <div className='p-[10px] mr-[1300px] ml-[30px] bg-white text-black hover:bg-yellow-300 hover:text-black transition duration-0 hover:duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
+        <img src={superStarTile} className=' h-[300px] w-[200px] rounded-[8px] xs:h-[200px] w-[200px]'/>
+       <span className='md:ml-[58px] xs:ml-[35px]'>AJ STYLES</span>
+       </div>
+      
     </div>
   )
 }
---
+++ src/data/wwestars.js
@@ -0,0 +1,46 @@
+import romantile from '../assets/romanantile.jpg'
+import sethtile from '../assets/sethtile.avif'
+import deantile from '../assets/dean1.jfif'
+import stylestile from '../assets/aj3.jfif'
+import hogantile from '../assets/hogantile.jpg'
+import samitile from '../assets/samitile.jpg'
+ const wwestars=[
+    {
+        name:'Roman Reigns',
+        imgLink:{romantile}
+    },
+    {
+        name:'Seth Rollins',
+        imgLink:{sethtile}
+    },
+    {
+        name:'Dean Ambrose',
+        imgLink:{deantile}
+    },
+    {
+        name:'Aj styles',
+        imgLink:{stylestile}
+    },
+    {
+        name:'Hulk Hogan',
+        imgLink:{hogantile}
+    },
+    {
+        name:'Sami Zayn',
+        imgLink:{samitile}
+    },
+    {
+        name:'John Cena',
+        imgLink:''
+    },
+    {
+        name:'Stone Cold',
+        imgLink:''
+    },
+    {
+        name:'Undertaker',
+        imgLink:''
+    }
+ 
+];
+export default wwestars;
