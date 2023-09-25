57f7ca42cc5c8a065f21b0d00f2721758bd92bdb
Aakash <aakash2506c@gmail.com>
2023-09-25 21:47:12 +0530

    mapping superstars card done

--- src/components/SuperStars.jsx
+++ src/components/SuperStars.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import superstarbanner from '../assets/UNIVERSAL_CHAMPIONSHIP_BANNER.jpg'
-import superStarTile from '../assets/SuperStar_tile3.png';
+import superStarTile from '../assets/stonecoldtile.jpg';
+import wwestars from '../data/wwestars';
 const SuperStars = () => {
   return (
-    <div className=' font-bebasNeue bg-black text-white'>
+    <div className=' h-[4000px] font-bebasNeue bg-black text-white'>
       <img src={superstarbanner} className='absoloute z-[-4] opacity-[0.8]'/>
       <h1 className='text-8xl absolute top-[320px] right-[200px] text-yellow-200'>SUPERSTARS</h1>
       <div className='ml-[700px] mt-[40px] text-3xl mr-[695px]'>WWE ROSTER</div>
-      <div className='p-[10px] mr-[1300px] ml-[30px] bg-white text-black hover:bg-yellow-300 hover:text-black transition duration-0 hover:duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
-        <img src={superStarTile} className=' h-[300px] w-[200px] rounded-[8px] xs:h-[200px] w-[200px]'/>
-       <span className='md:ml-[58px] xs:ml-[35px]'>AJ STYLES</span>
-       </div>
-      <div className='p-[10px] mr-[1300px] ml-[30px] bg-white text-black hover:bg-yellow-300 hover:text-black transition duration-0 hover:duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
-        <img src={superStarTile} className=' h-[300px] w-[200px] rounded-[8px] xs:h-[200px] w-[200px]'/>
-       <span className='md:ml-[58px] xs:ml-[35px]'>AJ STYLES</span>
-       </div>
+      <div className='grid grid-rows-3 grid-cols-3 gap-8 ml-[180px]  mt-[70px]'>
+      {wwestars.map((wwestar,index)=>{
+        return (
+         <div key={index} className='p-[10px] mr-[215px] ml-[30px]  bg-white text-black hover:bg-white hover:text-black transition duration-0 hover:duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
+         <img src={wwestar.imgLink} className='h-[300px] w-[200px] rounded-[8px] xs:h-[200px] w-[200px]'/>
+        <span className='md:ml-[38px] xs:ml-[35px] mx-auto'>{wwestar.name}</span>
+        </div>
+      )
+      })}
+     </div>
       
     </div>
   )
--- src/data/wwestars.js
+++ src/data/wwestars.js
@@ -1,45 +1,56 @@
-import romantile from '../assets/romanantile.jpg'
+
+import sup1 from '../assets/Roman2.jpg'
 import sethtile from '../assets/sethtile.avif'
 import deantile from '../assets/dean1.jfif'
 import stylestile from '../assets/aj3.jfif'
 import hogantile from '../assets/hogantile.jpg'
-import samitile from '../assets/samitile.jpg'
+import samitile from '../assets/samitile.webp'
+import cenatile from '../assets/cenatile.avif'
+import stonecoldtile from '../assets/stonecoldtile.jpg'
+import undertakertile from '../assets/undertakertile.jpg'
  const wwestars=[
     {
         name:'Roman Reigns',
-        imgLink:{romantile}
+        imgLink:sup1,
+        alt:'1'
     },
     {
         name:'Seth Rollins',
-        imgLink:{sethtile}
+        imgLink:sethtile,
+        alt:'1'
     },
     {
         name:'Dean Ambrose',
-        imgLink:{deantile}
+        imgLink:deantile,
+        alt:'1'
     },
     {
         name:'Aj styles',
-        imgLink:{stylestile}
+        imgLink:stylestile,
+        alt:'1'
     },
     {
         name:'Hulk Hogan',
-        imgLink:{hogantile}
+        imgLink:hogantile,
+        alt:'1'
     },
     {
         name:'Sami Zayn',
-        imgLink:{samitile}
+        imgLink:samitile,
+        alt:'1'
     },
     {
         name:'John Cena',
-        imgLink:''
+        imgLink:cenatile,
+        alt:'1'
     },
     {
         name:'Stone Cold',
-        imgLink:''
+        imgLink:stonecoldtile,
     },
     {
         name:'Undertaker',
-        imgLink:''
+        imgLink:undertakertile
     }
  
 ];
