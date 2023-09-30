51af942fd9ce70460371a0acc72b4a19d7770eb5
Aakash <aakash2506c@gmail.com>
2023-09-30 21:47:19 +0530

    ok

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,30 +11,30 @@ const Login = () => {
         console.log(data);
       }
   return (
-    <div className='bg-black h-[2000px] text-white font-roboto'>
+    <div className='bg-black h-[2000px] text-white'>
       <img src={loginbg} className='opacity-[0.8] '/>
         <h1 className='text-8xl font-bebasNeue mt-[160px] ml-[660px]  absolute top-[90px] right-[280px] text-yellow-200'>SIGN UP/ sign in</h1>
         {/* <h1 className='text-6xl font-bebasNeue mt-[160px] ml-[660px] mr-[705px]'>SIGN UP</h1> */}
       <div className=' w-[800px] h-[800px] ml-[350px] pt-[300px]'>
       <form action="" className='' onSubmit={handleSubmit(onSubmit)}>
         <div>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name" className='font-roboto'>Name</label>
             <input id='name' name='name' type='text' className='ml-[38px] pr-[100px]' {...register("name")}/>
         </div>
         <div className='mt-[30px]'>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" className='font-roboto'>Email</label>
             <input id='email' name='email' type='email' className='text-black ml-[38px] pr-[100px] '  {...register("email")}/>
         </div>
         <div className='mt-[30px]'>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password" className='font-roboto'>Password</label>
             <input id='password' name='password' type='password' className='text-black ml-[10px] pr-[100px]'  {...register("password")}/>
         </div>
         <div className='mt-[30px]'>  
-            <label htmlFor="name">Phone no</label>
+            <label htmlFor="name" className='font-roboto'>Phone no</label>
             <input id='name' name='phoneno' type='phone-no' className='text-black ml-[10px] pr-[100px]'  {...register("phoneno")}/>
         </div>
         <div className='mt-[30px]'>  
-            <label htmlFor="name">Phone no</label>
+            <label htmlFor="name" className='font-roboto'>Phone no</label>
             <input id='name' name='phoneno' type='phone-no' className='text-black ml-[10px] pr-[100px]'  {...register("phoneno")}/>
         </div>
         <button type="submit" className='bg-white text-black mt-[45px] ml-[140px] p-[0.45rem] rounded-xl'>Login</button>
