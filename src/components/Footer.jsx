9ad480003f20d868e2863cdf3f54d49cf7d76277
Aakash <aakash2506c@gmail.com>
2023-10-03 12:06:00 +0530

    register page fully responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import footerlogo1 from '../assets/njpw2.webp'
 import footerlogo2 from '../assets/njpw1.png'
 const Footer = () => {
   return (
-    <div className='bg-[#1d1d1d] mt-[-7px] flex text-white font-bebasNeue xs:text-[0.8rem] md:text-[1rem]z-[-6]'>
+    <div className='bg-[#1d1d1d]  flex text-white font-bebasNeue xs:text-[0.8rem] md:text-[1rem]xs:h-[100px]'>
       <img src={footerlogo} className='md:pt-[55px] md:pb-[70px] md:pl-[30px] md:ml-[10px] xs:pl-[20px]  xs:mt-[30px]  '/>
-      <img src={footerlogo1} className='w-[60px] h-[60px] md:ml-[50px] md:mt-[80px] xs:ml-[30px] xs:mt-[35px]'/>
+      <img src={footerlogo1} className='w-[60px] h-[60px] md:ml-[50px] md:mt-[80px] xs:ml-[30px] xs:mt-[34px]'/>
       <div className='cont-1 md:ml-[900px] md:mt-[70px] xs:ml-[130px] xs:mt-[28px] '>
        <div className='hover:cursor-pointer'>Contact us</div>
        <div className='hover:cursor-pointer'>About</div>
@@ -17,7 +17,7 @@ const Footer = () => {
        <div className='hover:cursor-pointer'>Privacy Policy</div>
        <div className='hover:cursor-pointer'>Cookie Policy</div>
       </div>
-      <div className='absolute xs:mt-[105px] xs:ml-[100px] md:mt-[175px] md:ml-[650px]'><span>&#169;</span>COPYRIGHT 2023||ACPRODUCTIONS-ALL RIGHTS RESERVED</div>
+      <div className='xs:bg-[#1d1d1d] absolute xs:mt-[95px] xs:ml-[0px] md:mt-[175px] md:ml-[650px] xs:h-[20px] xs:w-[502px] xs:pl-[133px]'><span>&#169;</span>COPYRIGHT 2023||ACPRODUCTIONS-ALL RIGHTS RESERVED</div>
     </div>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,11 +13,11 @@ const Register = () => {
       }
 
   return (
-    <div className='md:h-[1200px] md:pt-[100px] text-white bg-black xs:pb-[50px]'>
+    <div className='md:h-[1200px] md:pt-[100px] text-white bg-black  xs:h-[1000px]'>
       {/* <img src={loginbg} className='opacity-[0.8] absolute z-[-4] '/> */}
         <h1 className='md:text-8xl font-bebasNeue md:ml-[650px] md:mr-[630px] text-yellow-200 xs:ml-[210px] xs:text-4xl xs:mr-[200px] '>SIGN UP</h1>
         {/* <h1 className='text-6xl font-bebasNeue mt-[160px] ml-[660px] mr-[705px]'>SIGN UP</h1> */}
-      <div className=' bg-white text-black md:w-[720px] md:h-[830px] md:ml-[400px] md:pt-[80px] md:mt-[40px] shadow-md shadow-white'>
+      <div className=' bg-white text-black md:w-[720px] md:h-[860px] md:ml-[400px] md:pt-[80px] md:mt-[40px] shadow-md md:shadow-white xs:h-[870px]'>
       <form action="" className='xs:mt-[30px] xs:pt-[30px] md:mt-[0px]' onSubmit={handleSubmit(onSubmit)}>
       <div className='md:text-left xs:text-center'>
             <label htmlFor="email" className='xs:text-left md:font-roboto md:ml-[31px] xs:mr-[340px]'>Email *</label>
@@ -43,21 +43,21 @@ const Register = () => {
               minLength:4
             })}/>
         </div>
-        <div className='md:text-left xs:text-center mt-[30px]'>  
-            <label htmlFor="name" className='xs:text-left md:font-roboto md:ml-[31px] xs:mr-[315px]'>Phone no</label>
+        <div className='md:text-left xs:text-center mt-[30px] xs:mb-[10px]'>  
+            <label htmlFor="name" className='xs:text-left md:font-roboto md:ml-[31px] xs:mr-[315px] '>Phone no</label>
              <br/>
             <input id='name' name='phoneno' type='phone-no' className='xs:w-[386px] xs:h-[54px] xs:mt-[5px] md:ml-[30px] border-[4px] md:w-[660px] rounded-md md:h-[48px] md:mt-[5px] md:p-2'  {...register("phoneno")}/>
         </div>
         <div className='flex'>
-        <input type="checkbox" id="consent_checkbox" name="vehicle1" value="Bike" className='ml-[30px] h-[20px] mt-[18px]'></input>
+        <input type="checkbox" id="consent_checkbox" name="vehicle1" value="Bike" className='md:ml-[30px] h-[20px] mt-[18px] xs:ml-[58px] xs:mt-[5px]'></input>
         <label htmlFor='consent_checkbox'>
-        <p className='ml-[10px] w-[660px] rounded-md h-[96px] mt-[15px] '>
+        <p className='md:ml-[10px] md:w-[660px] rounded-md md:h-[96px] md:mt-[15px] xs:w-[363px] xs:h-[134px] xs:ml-[10px] '>
         I consent to receiving marketing communications from Wrestling World and its affiliates about special offers and other products or services based on my information and interests, including how I use and interact with the services, as described in the <a href='https://www.wwe.com/page/privacy-policy' target='_blank' className='text-blue-400'>Privacy Policy</a> . I can unsubscribe at any time using options available in the <a href='https://www.wwe.com/page/privacy-policy' target='_blank' className='text-blue-400'>Privacy Policy</a>.
         </p>
         </label>
         </div>
-        <p className='ml-[240px] pl-[6px] mt-[15px] mr-[240px]'>Have an account? <NavLink to='/login' className='text-blue-400' >Login </NavLink> instead</p>
-        <button type="submit" className='bg-black text-white md:mt-[25px] md:w-[660px] md:ml-[30px] md:p-[0.45rem] font-bebasNeue hover:bg-black hover:text-yellow-200  '>Sign up</button>
+        <p className='md:ml-[240px] md:pl-[6px] md:mt-[15px] md:mr-[240px] xs:mt-[50px] xs:ml-[130px]'>Have an account? <NavLink to='/login' className='text-blue-400' >Login </NavLink> instead</p>
+        <button type="submit" className='bg-black text-white md:mt-[25px] md:w-[660px] md:ml-[30px] p-[0.45rem] font-bebasNeue hover:bg-black hover:text-yellow-200 xs:mt-[20px] xs:ml-[60px] xs:w-[386px] xs:mb-[20px]'>Sign up</button>
       </form>
       </div>
     </div>
