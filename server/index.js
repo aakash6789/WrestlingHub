2f7cdfa14d1b73a8449e0532fcb7ad2b83d97373
Aakash <aakash2506c@gmail.com>
2023-10-06 20:00:52 +0530

    created server, running on port 3000

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// import {express} from express;
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("Great going!")
+})
+app.listen(3000, () => console.log('Server listening on port 3000!'));
