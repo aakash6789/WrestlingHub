f26ed4ced744568066d2a8a24306fa64ab974ef1
Aakash <aakash2506c@gmail.com>
2023-10-08 22:21:38 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -8,3 +8,4 @@ app.get('/',(req,res)=>{
 app.listen(3000, () => console.log('Server listening on port 3000!'));
 //server on 3000 client on- 
 //60% done just 40 more to go
+//aaksh1456c@ id for mongodb
