b7064e599c69ce17d3f023ebd657655ae18723b8
Aakash <aakash2506c@gmail.com>
2023-10-11 23:26:08 +0530

    Database connected!

--- server/index.js
+++ server/index.js
@@ -7,8 +7,14 @@ dotenv.config();
 app.get('/',(req,res)=>{
     res.send("Great going!");
 })
+//DB CONNECTION
 const port=process.env.PORT;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
 app.listen(port, () => console.log('Server listening on port 3000!'));
+}).catch((error)=>console.log(`${error}: DB did not connect`));
 //server on 3000 client on- 
 //60% done just 40 more to go
 //aaksh1456c@ id for mongodb
