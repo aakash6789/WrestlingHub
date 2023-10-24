51ecbe889eb810f9384576a2a434e69b81e20413
Aakash <aakash2506c@gmail.com>
2023-10-24 09:36:15 +0530

    ok

---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:"true",
+        unique:"true",
+        max:50
+    },
+    firstName:{
+        type:String,
+        required:"true",
+        min:2,
+        max:50
+    },
+    lastName:{
+        type:String,
+        required:"true",
+        min:2,
+        max:50
+    },
+    password:{
+        type:String,
+        required:"true",
+        min:5
+    },
+   phoneNo:{
+    type:Number,
+    min:10,
+    max:10
+   }
+})
+const User=mongoose.model("User",userSchema);
+export default User;
